19c16ff34d6593179310dec556216d10c2cbd6b9
Sofyan Egi <sofyanegil@gmail.com>
2025-07-30 21:23:07 +0700

    chore(deps): add next-toploader

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { Providers } from '@/components/providers';
 import { Toaster } from 'sonner';
+import NextTopLoader from 'nextjs-toploader';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,6 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <NextTopLoader />
         <Providers>{children}</Providers>
         <Toaster richColors />
       </body>
