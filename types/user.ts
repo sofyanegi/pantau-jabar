d33ba40fa1764fb853c9cac378914097ca5d918a
Sofyan Egi <sofyanegil@gmail.com>
2025-07-31 20:24:02 +0700

    feat(user): add user form validation schema using Zod for user input validation

--- types/user.ts
+++ types/user.ts
@@ -1,6 +1,17 @@
+import { z } from 'zod';
+
 export interface User {
   id: string;
   name: string;
   email: string;
   role: 'ADMIN' | 'USER';
 }
+
+export const userFormSchema = z.object({
+  id: z.string().optional(),
+  name: z.string().min(1, 'Nama wajib diisi'),
+  email: z.string().email('Email tidak valid'),
+  role: z.enum(['ADMIN', 'USER'], 'Role Wajib diisi'),
+});
+
+export type UserFormValues = z.infer<typeof userFormSchema>;
